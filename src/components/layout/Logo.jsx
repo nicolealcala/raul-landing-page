b8a75235efd6cd65c74addddaa4e7a14198f423b
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 10:18:47 +0800

    Add nav icons and logo component

---
+++ src/components/layout/Logo.jsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="flex gap-x-2 items-center">
+      <div className="size-6 bg-primary-400 rounded-full"></div>
+      <h1 className="font-extrabold text-2xl">RAUL</h1>
+    </div>
+  );
+}
