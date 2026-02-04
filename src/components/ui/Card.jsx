d260af05922ee44f3c57d34ca33e6f9bef0189c7
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 14:46:14 +0800

    Update breakpoint styling

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -4,8 +4,9 @@ export default function Card({ header, title, description, className }) {
   return (
     <div
       className={cn(
-        "rounded-2xl flex flex-col gap-y-6 px-8 md:px-12 py-7 pb-12 bg-white w-full shrink-0 max-w-full md:max-w-90 self-start",
+        "rounded-2xl flex flex-col gap-y-6 px-8 md:px-12 py-7 pb-12 bg-white w-full shrink-0 self-start",
         "shadow-[0px_0px_20px_2px_rgba(0,0,0,0.08)]",
+        "max-w-full lg:max-w-90",
         className,
       )}
     >
