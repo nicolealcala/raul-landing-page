1ea550cdb763f1211acefb890e7a6e0aee7a4fbe
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 17:04:26 +0800

    Maintenance and Not found pages

---
+++ src/pages/MaintenancePage.jsx
@@ -0,0 +1,12 @@
+import Button from "../components/ui/Button";
+
+export default function MaintenancePage() {
+  return (
+    <div className="max-w-7xl mx-auto flex-center flex-col gap-y-7">
+      <p>Page is under development</p>
+      <Button component="a" link="/">
+        Go back Home
+      </Button>
+    </div>
+  );
+}
