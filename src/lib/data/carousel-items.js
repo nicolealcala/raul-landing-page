cbb040dd0f06b9b905355833ada446448451cfe4
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 14:46:44 +0800

    Remove title periods

--- src/lib/data/carousel-items.js
+++ src/lib/data/carousel-items.js
@@ -1,7 +1,7 @@
 export const carouselItems = [
     { title: "Thrive in a digital first world", description: "From marketing strategies and brand development to business consulting and digital." },
-    { title: "Let's turn your vision into reality.", description: "From marketing strategies and brand development to business consulting and digital" },
-    { title: "Where innovation meets strategy.", description: "From marketing strategies and brand development to business consulting and digital" },
-    { title: "This is a filler card.", description: "From marketing strategies and brand development to business consulting and digital" },
-    { title: "Another filler card.", description: "From marketing strategies and brand development to business consulting and digital" }
+    { title: "Let's turn your vision into reality", description: "From marketing strategies and brand development to business consulting and digital" },
+    { title: "Where innovation meets strategy", description: "From marketing strategies and brand development to business consulting and digital" },
+    { title: "This is a filler card", description: "From marketing strategies and brand development to business consulting and digital" },
+    { title: "Another filler card", description: "From marketing strategies and brand development to business consulting and digital" }
 ]
