7c3bf770dbb6931347dd47db550f99aa52085fab
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 20:35:12 +0800

    Update Logo component

--- src/components/layout/Logo.jsx
+++ src/components/layout/Logo.jsx
@@ -1,9 +1,7 @@
-import LogoIcon from "../icons/LogoIcon";
-
 export default function Logo() {
   return (
     <div className="flex gap-x-4 items-center">
-      <LogoIcon className="size-8" />
+      <img src="/favicon.svg" alt="Raul Icon" className="size-8" />
       <h1 className="font-extrabold text-2xl">RAUL</h1>
     </div>
   );
