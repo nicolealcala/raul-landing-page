8bee5329df16c87ba87df7ca255a0bd98b41ffa0
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 18:01:09 +0800

    moved gap as padding on main

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -5,9 +5,9 @@ import { Suspense } from "react";
 
 export default function RootLayout() {
   return (
-    <div className="min-h-dvh max-w-screen overflow-hidden flex flex-col gap-y-24 py-6 px-4 md:py-9 md:px-6 xl:px-0 text-left">
+    <div className="min-h-dvh max-w-screen overflow-hidden flex flex-col py-6 px-4 md:py-9 md:px-6 xl:px-0 text-left">
       <Navbar />
-      <main id="main-content" className="flex-1 flex-center">
+      <main id="main-content" className="flex-1 flex-center py-24 ">
         <Suspense
           fallback={<div className="h-screen flex-center">Loading...</div>}
         >
