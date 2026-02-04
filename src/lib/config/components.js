477662ed06fb94d555ebc6c7d31f2a2ac18d7bd9
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 14:46:31 +0800

    Update ghist variant styling

--- src/lib/config/components.js
+++ src/lib/config/components.js
@@ -1,6 +1,6 @@
 export const buttonVariants = {
     filled: "bg-primary-400 text-white hover:bg-primary-500",
-    ghost: "bg-transparent hover:bg-primary-50/50"
+    ghost: "bg-transparent hover:bg-primary-100/30"
 }
 
 export const buttonShapes = {
