41f65c890a8724151c56897556cb96d2a81bd40b
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-04 17:04:43 +0800

    Add suspense wrapper

--- src/components/layout/RootLayout.jsx
+++ src/components/layout/RootLayout.jsx
@@ -1,11 +1,19 @@
+import { Outlet } from "react-router";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
+import { Suspense } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   return (
     <div className="min-h-dvh max-w-screen overflow-hidden flex flex-col gap-y-24 py-6 px-4 md:py-9 md:px-6 xl:px-0 text-left">
       <Navbar />
-      <main>{children}</main>
+      <main id="main-content" className="flex-1 flex-center">
+        <Suspense
+          fallback={<div className="h-screen flex-center">Loading...</div>}
+        >
+          <Outlet />
+        </Suspense>
+      </main>
       <Footer />
     </div>
   );
