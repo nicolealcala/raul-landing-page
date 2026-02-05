048b5be09a305aaa30aa1e17fe11fc42a87d58d2
Nicole Alcala <nicolealcalq@gmail.com>
2026-02-05 11:03:36 +0800

    Add back to top button

---
+++ src/components/ui/BackToTop.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import ArrowUp from "../icons/ArrowUp";
+import IconButton from "./IconButton";
+
+export default function BackToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function toggleVisibility() {
+    if (window.scrollY > 200) setIsVisible(true);
+    else setIsVisible(false);
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  return isVisible ? (
+    <IconButton
+      aria-label="Scroll to Top"
+      variant="filled"
+      className="fixed bottom-5 right-5 lg:bottom-10 lg:right-10 z-80"
+      onClick={scrollToTop}
+    >
+      <ArrowUp color="white" />
+    </IconButton>
+  ) : null;
+}
